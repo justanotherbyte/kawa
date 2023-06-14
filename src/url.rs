7b4678e5e582e4989329fde6bcde2f0b1cc1bb68
justanotherbyte <wishymovies@gmail.com>
2023-06-14 21:52:51 +0100

    rewrite url parser

--- src/url.rs
+++ src/url.rs
@@ -18,58 +18,66 @@ impl Url {
 #[pymethods]
 impl Url {
     // http(s)://127.0.0.1:8080/
+    // https://www.google.com
+    // https://google.com
     #[new]
     pub fn parse(url: String) -> PyResult<Self> {
         let supported_schemes: Vec<&str> = vec!["http", "https", "ws", "wss"];
-
-        let components = url.split("://").collect::<Vec<&str>>();
-        let scheme = components[0];
-        let parts = components[1];
+        let scheme_other_split: Vec<&str> = url.split("://").collect();
+        let (scheme, other) = (scheme_other_split[0], scheme_other_split[1]);
 
         assert!(supported_schemes.contains(&scheme));
 
-        // now we are left with just the 127.0.0.1:8080/ section to deal with
-        let host_port = parts.split(':').collect::<Vec<&str>>();
-        let (host, port_part) = (host_port[0], host_port[1]);
-
-        // now we need to extract the port section
-        // there COULD be a path, there might not be
-        // if there isn't a path, we simply assume its "/"
-
-        let mut port_path = port_part.split('/').collect::<Vec<&str>>();
-        let port: usize = port_path[0].parse()?;
-        port_path.remove(0);
+        let mut port: usize = match scheme {
+            "https" => 443,
+            "wss" => 443,
+            "http" => 80,
+            "ws" => 80,
+            _ => 443 // we just assume its 443 if we somehow got here
+        };
 
         let mut path: String = "/".into();
+        let mut host: String;
 
-        if !port_path.is_empty() {
-            // seems like there was a path
-            if port_path.len() > 1 {
-                // we have a path with the following structure: /x/y/z
-                let mut path_inner = port_path.join("/");
-                if !path_inner.starts_with('/') {
-                    path_inner = format!("/{path_inner}");
-                }
-                path = path_inner;
-            } else {
-                // we have a single path: /x
-                let mut path_inner = port_path[0].to_string();
-                if !path_inner.is_empty() {
-                    if !path_inner.starts_with('/') {
-                        path_inner = format!("/{path_inner}");
-                    }
-                    path = path_inner;
-                }
+        // we're now left with something like this:
+        // - (www.)google.com(:80)(/(path))
+
+        // first lets see if a path exists
+        // if not we assume its "/"
+
+        let mut other_path_split: Vec<&str> = other.split('/').collect();
+        if other_path_split.len() != 1 {
+            // there could be a path
+            // "/" exists in the url
+            host = other_path_split.remove(0).into();
+            let possible_path = other_path_split[0];
+
+            if !possible_path.is_empty() {
+                let joined_back_path = other_path_split.join("/");
+                path = format!("/{joined_back_path}"); // we need the leading slash
             }
+        } else {
+            host = other_path_split[0].into();
         }
 
-        let url = Self {
-            host: host.into(),
+        // now we parse out the port from the host
+        if host.contains(':') {
+            // contains a colon, possible new port
+            let host_port_split: Vec<&str> = host.split(':').collect();
+            let new_host = host_port_split[0].into();
+            let new_port: usize = host_port_split[1].parse()?;
+            port = new_port;
+            host = new_host;
+        }
+
+        let url_struct = Self {
+            host,
             port,
-            scheme: scheme.into(),
             path,
+            scheme: scheme.into()
         };
-        Ok(url)
+        
+        Ok(url_struct)
     }
 
     #[getter]
