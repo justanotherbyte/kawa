700057ce785b27776262fe771b97fc27c5f4cc73
justanotherbyte <wishymovies@gmail.com>
2023-06-14 18:27:05 +0100

    rewrite tests to use the new helper methods

--- tests/client/test_http.py
+++ tests/client/test_http.py
@@ -2,7 +2,7 @@ import kawa
 
 
 def test_plaintext():
-    response = kawa.request("GET", "http://localhost:8080")
+    response = kawa.get("http://localhost:8080")
     assert response.body == "hello world"
 
 def test_json():
@@ -12,8 +12,7 @@ def test_json():
         "username": "justanotherbyte",
         "password": "http"
     }
-    response = kawa.request(
-        "POST",
+    response = kawa.post(
         "http://127.0.0.1:8080/data",
         headers={"Content-Type": "application/json"},
         body=json.dumps(payload)
